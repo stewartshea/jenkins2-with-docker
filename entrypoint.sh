0fccc8f8c65a6cb529aa828659cfb4e084b35211
Shea Stewart <stewart.shea@gmail.com>
2019-07-02 10:20:21 -0700

    test kube cred holding

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-oc login --token $DOCKER_PASSWORD $OPENSHIFT_URL
+oc login --token $DOCKER_PASSWORD $OPENSHIFT_URL 
+cat ~/.kube/config
+mkdir -p $HOME/.kube
+cp ~/.kube/config $HOME/.kube/config

42dadab7bd4f9b7013182a9db68db28c55150604
Shea Stewart <stewart.shea@gmail.com>
2019-07-02 10:30:42 -0700

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+export KUBECONFIG=$HOME/kube
+
 oc login --token $DOCKER_PASSWORD $OPENSHIFT_URL 
-cat ~/.kube/config
-mkdir -p $HOME/.kube
-cp ~/.kube/config $HOME/.kube/config
+cat $KUBECONFIG
+
